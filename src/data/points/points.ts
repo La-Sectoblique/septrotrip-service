f861e6ec5dbfeeaf3f3269fd1b2101e5503a09d1
Tanguy Gimenez <gimeneztanguy@gmail.com>
2022-03-10 11:50:25 +0100

    feat(point): adding update and delete functions

--- src/data/points/points.ts
+++ src/data/points/points.ts
@@ -2,6 +2,8 @@ import axios from "axios";
 import { request } from "../../utils/Request";
 import InvalidBodyError from "../../types/errors/InvalidBodyError";
 import { isPointOutput, isPointOutputArray, PointInput, PointOutput } from "../../types/models/Point";
+import NoIdProvidedError from "../../types/errors/NoIdProvidedError";
+import InexistantResourceError from "../../types/errors/InexistantResourceError";
 
 export async function addPoint(data: Omit<PointInput, "authorId">): Promise<PointOutput> {
 
@@ -39,6 +41,60 @@ export async function getUserPoints(): Promise<PointOutput[]> {
 		}
 	}
 	catch(error) {
+		throw error;	
+	}
+}
+
+export async function updatePoint(id: number, data: PointInput) {
+
+	try {
+		const response = (await request(`/points/${id}`, "PUT", data));
+
+		if(isPointOutput(response)) {
+			return response;
+		}
+		else {
+			throw new Error(JSON.stringify(response));
+		}
+	}
+	catch(error) {
+
+		if(axios.isAxiosError(error)) {
+			if(error.response?.status === 400) {
+				throw { message: error.response?.data.message, code: 400, name: "NoIdProvidedError" } as NoIdProvidedError;
+			}
+			else if (error.response?.status === 404) {
+				throw { message: error.response?.data.message, code: 404, name: "InexistantResourceError" } as InexistantResourceError;
+			}
+		}
+
+		throw error;	
+	}
+}
+
+export async function deletePoint(id: number) {
+
+	try {
+		const response = (await request(`/points/${id}`, "DELETE"));
+
+		if(isPointOutput(response)) {
+			return response;
+		}
+		else {
+			throw new Error(JSON.stringify(response));
+		}
+	}
+	catch(error) {
+
+		if(axios.isAxiosError(error)) {
+			if(error.response?.status === 400) {
+				throw { message: error.response?.data.message, code: 400, name: "NoIdProvidedError" } as NoIdProvidedError;
+			}
+			else if (error.response?.status === 404) {
+				throw { message: error.response?.data.message, code: 404, name: "InexistantResourceError" } as InexistantResourceError;
+			}
+		}
+
 		throw error;	
 	}
 }
