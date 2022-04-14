563de1cf8a5449c0a54399edacf05433139c2d7c
ladwein <samuel.ladwein@etu.unistra.fr>
2022-04-14 09:43:08 +0200

    fix(): Sam mistake

--- src/data/points/points.ts
+++ src/data/points/points.ts
@@ -14,6 +14,7 @@ import { isApiResponse } from "../../types/utils/Api";
  */
 export async function addPoint(tripId: number, data: Omit<PointInput, "authorId" | "tripId">): Promise<PointOutput> {
 
+	
 	try {
 		const response = (await request(`/trips/${tripId}/points`, "POST", data));
 
